721de79945acbd32be9da53c42b0fba142b7bd10
hridyesh-dev <hridyesh.dev@gmail.com>
2025-12-19 16:20:22 +0530

    project end game

--- Social_Media_Project/backend/src/controllers/posts.controller.js
+++ Social_Media_Project/backend/src/controllers/posts.controller.js
@@ -8,11 +8,20 @@
 
 */
 
-export async  function createPostController( req , res ){
-    //req.file mai image aae gi 
-    const file=req.file
-    //yeh file deni hai ai ko aur iss file se caption banani hai 
-    
+import { generateCaption } from "../service/ai.service.js";
 
+export async  function createPostController( req , res ){
+    
+    //req.file mai image aae gi 
+    const file = req.file
+
+    //yeh file deni hai ai ko aur iss file se caption banani hai 
+    console.log("file received ",file)
+
+    // buffer is the actual file data that we try to upload on image kit 
+    const base64Image= new Buffer.from(file.buffer).toString('base64')    
+
+    const caption=await generateCaption(base64Image)
+    res.json({caption});
 
 }
--- Social_Media_Project/backend/src/service/ai.service.js
+++ Social_Media_Project/backend/src/service/ai.service.js
@@ -1,16 +1,44 @@
+/*
+    types of genAi
+    text to text , text to image , text to video , text to audio 
+    image upload kare ----> caption mile 
+
+    if image file in less <20mb wih prompt we can do inline method 
+    or else we can use file API 
+
+
+*/
+
 import { GoogleGenAI } from "@google/genai";
 
-const ai = new GoogleGenAI({
-    apiKey: "",
-});
+const ai = new GoogleGenAI({});
 
+//base 64 mai input dena caption mill jaaye gi 
+export async function generateCaption(base64ImageFile){
+    const contents = [
+        {
+            //image ko de rahe hai 
+            inlineData: {
+                mimeType: "image/jpeg",
+                data: base64ImageFile,
+            },
+        },
+        { text: "caption this image ." },
+    ];
 
-async function main() {
+    //giving image as input and returning the caption 
     const response = await ai.models.generateContent({
         model: "gemini-2.5-flash",
-        contents: "Explain how AI works in a few words",
+        contents: contents,
+        config:{
+            systemInstruction:`
+                You are an expert in generating captions for images
+                you generate single caption for the image.
+                Your caption should be short and concise
+                and You use hashtags and emojis in the caption
+            `
+        }
     });
-    console.log(response.text);
+    console.log(response);
+    return response.text;
 }
-
-await main();
